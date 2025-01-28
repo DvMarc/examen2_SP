c9b770e665d53553d4967d35a60faa2420b5f543
Rommel David Marcillo Pilay <romdmarc@espol.edu.ec>
2025-01-28 15:38:18 -0500

    descripciones de los plots

--- main.py
+++ main.py
@@ -38,15 +38,19 @@ elif st.session_state["opcion"] == "Plots":
 
     fig1 = px.line(df, x='DATEPRD', y='BORE_OIL_VOL', color='WELL_BORE_CODE', title='Volumen de petróleo vs Tiempo por Pozo')
     st.plotly_chart(fig1)
+    st.write('En esta gráfica se muestra el volumen de petróleo producido por pozo a lo largo del tiempo.')
 
     fig2 = px.line(df, x='DATEPRD', y='BORE_GAS_VOL', color='WELL_BORE_CODE', title='Volumen de gas vs Tiempo por Pozo')
     st.plotly_chart(fig2)
+    st.write('En esta gráfica se muestra el volumen de gas producido por pozo a lo largo del tiempo.')
 
     fig3 = px.line(df, x='DATEPRD', y=['BORE_OIL_VOL', 'BORE_WAT_VOL'], title='Volumen de petróleo y agua vs Tiempo')
     st.plotly_chart(fig3)
+    st.write('En esta gráfica se muestra el volumen de petróleo y agua producido a lo largo del tiempo.')
 
     total_volumes = df.groupby('WELL_BORE_CODE')[['BORE_OIL_VOL', 'BORE_GAS_VOL', 'BORE_WAT_VOL']].sum().reset_index()
     fig4 = px.bar(total_volumes, x='WELL_BORE_CODE', y=['BORE_OIL_VOL', 'BORE_GAS_VOL', 'BORE_WAT_VOL'], title='Producción total por pozo', barmode='group')
     st.plotly_chart(fig4)
+    st.write('En esta gráfica se muestra la producción total de petróleo, gas y agua por pozo.')
 
 
